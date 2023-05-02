62ef5d797d16fc4f3e0eb7cdbe6c358a97ea7d95
ltran7233 <ltran7233@LAPTOP-AAAUJLF3>
2023-05-02 07:21:35 -0400

    added final tests

--- main.cpp
+++ main.cpp
@@ -5,7 +5,52 @@
 int main()
 {
 	StackList *s1 = new StackList();
-	Queue *q1 = new Queue();
 	std::cout << s1->is_empty() << std::endl;
+	s1->push("1");
+	s1->push("2");
+	s1->push("3");
+	std::cout << s1->top() << std::endl;
+	std::cout << s1->pop() << std::endl;
+	s1->push("4");
+	std::cout << s1->top() << std::endl;
+	std::cout << s1->pop() << std::endl;
 	
+	StackList *e = new StackList(); // empty stack
+	try
+	{
+		std::cout << e->top() << "\n";
+	}
+	catch (int err)
+	{
+		std::cout << "stack empty error: " << err << "\n";
+	}
+
+	try
+	{
+		std::cout << e->pop() << "\n";
+	}
+	catch (int err)
+	{
+		std::cout << "stack empty error: " << err << "\n";
+	}
+	
+	Queue *q1 = new Queue();
+	std::cout << q1->is_empty() << std::endl;
+	std::cout << q1->is_full() << std::endl;
+	q1->enqueue("2");
+	q1->enqueue("4");
+	q1->enqueue("6");
+	q1->enqueue("8");
+	std::cout << q1->front() << std::endl;
+	q1->enqueue("10");
+	q1->enqueue("12");
+	q1->enqueue("14");
+	q1->enqueue("16");
+	q1->enqueue("18");
+	q1->enqueue("20"); // full
+	std::cout << q1->is_full() << std::endl;
+	for (int i=0; i<10; i++){
+		q1->dequeue();
+	}
+	std::cout << q1->is_empty() << std::endl;
 }
--- tests.cpp
+++ tests.cpp
@@ -20,3 +20,27 @@ TEST_CASE("stack functions"){
 	CHECK(s1->top() == "4");
 	CHECK(s1->pop() == "2");
 }
+
+TEST_CASE("queue functions"){
+	CHECK(q1->is_empty() == 1);
+	CHECK(q1->is_full() == 0);
+	q1->enqueue("2");
+	q1->enqueue("4");
+	q1->enqueue("6");
+	q1->enqueue("8");
+	CHECK(q1->front() == "2");
+	q1->enqueue("10");
+	q1->enqueue("12");
+	q1->enqueue("14");
+	q1->enqueue("16");
+	q1->enqueue("18");
+	q1->enqueue("20"); // full
+	CHECK(q1->is_full() == 1);
+	CHECK(q1->is_empty() == 0);
+	q1->dequeue();
+	CHECK(q1->front() == "4");
+	for (int i=0; i<9; i++){
+		q1->dequeue();
+	}
+	CHECK(q1->is_empty() == 1);
+}
