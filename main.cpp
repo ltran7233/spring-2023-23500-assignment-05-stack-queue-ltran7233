b207dc14c4955051afee153cc74a095977028fcd
ltran7233 <ltran7233@LAPTOP-AAAUJLF3>
2023-05-02 07:08:06 -0400

    added tests cases

--- main.cpp
+++ main.cpp
@@ -6,4 +6,6 @@ int main()
 {
 	StackList *s1 = new StackList();
 	Queue *q1 = new Queue();
+	std::cout << s1->is_empty() << std::endl;
+	
 }
--- tests.cpp
+++ tests.cpp
@@ -10,5 +10,13 @@ TEST_CASE("stack isempty()"){
 	CHECK(s1->is_empty() == true);
 }
 
-TEST_CASE(){
+TEST_CASE("stack functions"){
+	s1->push("1");
+	s1->push("2");
+	s1->push("3");
+	CHECK(s1->top() == "3");
+	CHECK(s1->pop() == "2");
+	s1->push("4");
+	CHECK(s1->top() == "4");
+	CHECK(s1->pop() == "2");
 }
